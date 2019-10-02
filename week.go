a58335d57bf6921695e499b28edd5f5e89be8a40
Jorge Inostroza <jeinostroza@hotmail.com>
2019-10-02 15:27:24 -0500

    add for

--- week.go
+++ week.go
@@ -7,27 +7,33 @@ import (
 func main() {
 	var day string = ""
 	var dayWords = ""
+	var option string = "y"
 
-	fmt.Println("Please enter a number between 1 and 7")
+	for option == "y" {
 
-	fmt.Scanln(&day)
+		fmt.Println("Please enter a number between 1 and 7")
 
-	switch day {
-	case "1":
-		dayWords = "Monday"
-	case "2":
-		dayWords = "Tuesday"
-	case "3":
-		dayWords = "Wednesday"
-	case "4":
-		dayWords = "Thursday"
-	case "5":
-		dayWords = "Friday"
-	case "6":
-		dayWords = "Saturday"
-	case "7":
-		dayWords = "Sunday"
+		fmt.Scanln(&day)
+
+		switch day {
+		case "1":
+			dayWords = "Monday"
+		case "2":
+			dayWords = "Tuesday"
+		case "3":
+			dayWords = "Wednesday"
+		case "4":
+			dayWords = "Thursday"
+		case "5":
+			dayWords = "Friday"
+		case "6":
+			dayWords = "Saturday"
+		case "7":
+			dayWords = "Sunday"
+		}
+
+		fmt.Println("The date is " + dayWords)
+		fmt.Println("Do you want other day(y/n)")
+		fmt.Scanln(&option)
 	}
-
-	fmt.Println("The date is " + dayWords)
 }
